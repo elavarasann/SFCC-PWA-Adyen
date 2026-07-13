e8fa0215a5c69b4dd6cdda7fa6b5ba86eaafa4c7
Ela <ela.vikatan@gmail.com>
2026-07-13 19:43:30 +0530

    Add PWA Kit Babel configuration

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+/*
+ * PWA Kit requires this root configuration when compiling the inherited
+ * Retail React App source from an overrides-only project.
+ */
+module.exports = require('@salesforce/pwa-kit-dev/configs/babel/babel-config')
